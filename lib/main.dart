fcb6573c7cb7af4717cf665ea7ec597e0eead1e3
muammar200 <amuammar01@gmail.com>
2024-03-09 23:48:24 +0800

    Section 03 - 02 Widget Dasar

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(BebasApp());
 }
 
-class MyApp extends StatelessWidget {
+class BebasApp extends StatelessWidget {
+  const BebasApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // backgroundColor: Colors.red.shade100,
-        body: Center(child: Text("Hello World!")),
-      ),
+          appBar: AppBar(
+            title: Text('Aplikasi Pertamaku',
+                style: TextStyle(color: Colors.white)),
+            centerTitle: true,
+            // backgroundColor: Color.fromARGB(255, 81, 165, 255),
+            backgroundColor: Colors.blue.shade500,
+          ),
+          body: Center(
+            // child: Image(image: AssetImage("assets/images/image1.jpg")),
+            //     child: Image(
+            //   image: NetworkImage("https://picsum.photos/id/237/200/300"),
+            // )
+            // child: Image.network("https://picsum.photos/id/237/200/300"),
+            child: Image.asset("assets/images/image1.jpg"),
+          )),
     );
   }
 }
